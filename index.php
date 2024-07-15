e892a7cb5eaf2f8ee89e8ff868dcbdf4a9c763e6
PedroVGelli <kaicribeiro015@gmail.com>
2024-07-15 03:42:57 -0300

    API de League of Legends

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$championUrl = "https://ddragon.leagueoflegends.com/cdn/12.18.1/data/pt_BR/champion.json";
+
+$resposta = file_get_contents($championUrl);
+$data = json_decode($resposta, true);
+
+$campeoes = $data['data'];
+
+$championKeys = array_keys($campeoes);
+shuffle($championKeys); 
+$Campeaoaleatorio = array_slice($championKeys, 0, 10); 
+?>
+
+<?php 
+require_once '_cabecalho.php'
+?>
+<body>
+    <div class="container">
+        <h1>10 Campeões Aleatórios</h1>
+        <table>
+            <div>
+                <tr>
+                    <th>Nome</th>
+                    <th>Função</th>
+                    <th>Imagem</th>
+                </tr>
+            </div>
+            <div>
+                <?php foreach ($Campeaoaleatorio as $key): ?>
+                    <?php $campeao = $campeoes[$key]; ?>
+                    <tr>
+                        <td><?php echo $campeao['name']; ?></td>
+                        <td><?php echo implode(", ", $campeao['tags']); ?></td>
+                        <td>
+                            <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/<?php echo $campeao['id']; ?>_0.jpg" alt="<?php echo $campeao['name']; ?>" width="100">
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </div>
+        </table>
+    </div>
+</body>
+</html>
